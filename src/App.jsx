25e4800691c21b5c915029a212dc7d9c9dbbf74c
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-23 14:41:19 -0300

    tours fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import { useState, useEffect } from 'react';
 
-import Loading from './Loading';
-import Tours from './Tours.jsx';
+import { Loading } from './Loading';
+import { Tours } from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setIsLoading(false);
+      setTours(data);
+    } catch (error) {
+      setIsLoading(false);
+      throw new Error(`Error: ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    // fetchTours se ejecuta cuando el componente se renderiza
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
